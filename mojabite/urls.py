60c342b31f674079c7f2cc27b7e5afa7366d03bf
antor <antorzuck>
2024-09-15 12:22:02 +0600

    basic changes

--- mojabite/urls.py
+++ mojabite/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from base.views import *
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
     path('view', view_item)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
